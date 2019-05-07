9d1bd7ce0e442997c5b8c2be5e085bfd4e1d6994
pkrisko <patrick.krisko@gmail.com>
2019-05-07 00:56:51 -0400

    refactored ternary

--- src/js/Bird.js
+++ src/js/Bird.js
@@ -86,9 +86,15 @@ class Bird {
             secondData = pipes.head.next.data,
             beforeLeftFirstPipe;
             // need to calculate topy and bottom y of next 3 vertical lines with pipe
-        let closest = (beforeLeftFirstPipe) ? headData : { x: headData.x + 80, yTop: headData.yTop, yBottom: headData.yBottom };
-        let secondClosest = (beforeLeftFirstPipe) ? { x: headData.x + 80, yTop: headData.yTop, yBottom: headData.yBottom} : secondData;
-        let thirdClosest = (beforeLeftFirstPipe) ? secondData : { x: secondData.x + 80, yTop: secondData.yTop, yBottom: secondData.yBottom };
+        let closest = (beforeLeftFirstPipe)
+                    ? headData
+                    : { x: headData.x + 80, yTop: headData.yTop, yBottom: headData.yBottom };
+        let secondClosest = (beforeLeftFirstPipe)
+                    ? { x: headData.x + 80, yTop: headData.yTop, yBottom: headData.yBottom}
+                    : secondData;
+        let thirdClosest = (beforeLeftFirstPipe)
+                    ? secondData
+                    : { x: secondData.x + 80, yTop: secondData.yTop, yBottom: secondData.yBottom };
         // Now create the inputs to the neural network
         let inputs = [];
         // x position of closest pipe
