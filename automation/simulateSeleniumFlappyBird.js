7d2f9bd9b4b5fccbcb19936f1d495f945e175f7b
pkrisko <patrick.krisko@gmail.com>
2019-05-21 14:47:29 -0400

    made it so 4 browsers are initiated

--- automation/simulateSeleniumFlappyBird.js
+++ automation/simulateSeleniumFlappyBird.js
@@ -1,21 +1,18 @@
 const webdriver = require('selenium-webdriver');
 
-const chromeDriver1 = new webdriver.Builder().forBrowser('chrome').build();
-const chromeDriver2 = new webdriver.Builder().forBrowser('chrome').build();
+for (let idx = 0; idx < 4; idx++) {
+    const chromeDriver = new webdriver.Builder().forBrowser('chrome').build();
+    navigateToBuiltURL(chromeDriver);
+}
 
-searchTest(chromeDriver1);
-searchTest(chromeDriver2);
-
-function searchTest(driver) {
-  driver.get('https://pkrisko.github.io/flappy-bird/build/index.html');
-  driver.sleep(2000).then(() => {
-    driver.getTitle().then(title => {
-      if(title === 'Flappy Bird')
-        console.log('Test passed');
-      else
-        console.log('Test failed');
-    }).catch(err => {
-      console.err('Error initializing the browser.');
+function navigateToBuiltURL(driver) {
+    driver.get('https://pkrisko.github.io/flappy-bird/build/index.html');
+    driver.sleep(2000).then(() => {
+        driver.getTitle().then(title => {
+            if(title === 'Flappy Bird')
+                console.log('Test passed');
+            else
+                console.log('Test failed');
+        });
     });
-  });
 }
