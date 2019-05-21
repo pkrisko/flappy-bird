5a8d4b08b0486c9ae460c5c427531ca4fbf3b3a0
pkrisko <patrick.krisko@gmail.com>
2019-05-20 22:52:46 -0400

    added a few steps

--- automation/runBrowsers.sh
+++ automation/runBrowsers.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-echo Hello World
-
 echo PREREQUISITES
 sudo apt-get update
 sudo apt-get install -y unzip xvfb libxi6 libgconf-2-4
-sudo apt-get install default-jdk
 
 echo INSTALL GOOGLE CHROME
 sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
@@ -15,6 +12,23 @@ sudo apt-get -y install google-chrome-stable
 echo INSTALL CHROMEDRIVER
 wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
+
+echo INSTALL NPM
+sudo apt install nodejs npm
+
+echo INSTALL GIT
+sudo apt-get install git-core
+
+echo CLONE AND INSTALL THE NECESSARY
+git clone https://github.com/pkrisko/flappy-bird.git
+
+echo INSTALL NPM DEPENDENCIES IN AUTOMATION FOLDER
+cd flappy-bird/automation
+npm install
+
+echo RUN THEM JEWELS FAST
+node simulateSeleniumFlappyBird.js
+
 
 
 
